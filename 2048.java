7d7bded30367b8782fb0acc8287ccebf83312542
ChengAaron <48865696+ChengAaron@users.noreply.github.com>
2019-12-17 11:06:18 -0500

    Add files via upload

---
+++ 2048.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
+
+public class 2048 extends JFrame{
+
+    public static void 2048(String[]args) {
+    	int[][]board = {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
+    }
+}
