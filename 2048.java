a555f0a34e2b3215a3d25febfb98a5d6ebba98a7
ChengAaron <48865696+ChengAaron@users.noreply.github.com>
2019-12-17 11:20:36 -0500

    Update 2048.java

--- 2048.java
+++ 2048.java
@@ -1,3 +1,4 @@
+import java.util.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -8,6 +9,20 @@ import javax.imageio.*;
 public class 2048 extends JFrame{
 
     public static void 2048(String[]args) {
-    	int[][]board = {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
+    	board = new board(int[][]board = {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}});
     }
+}
+
+class game {
+	Random rand = new Random();
+	private int[] startPiece = {2,4};
+	private int randX;
+	private int randY;
+	
+	public board(int[][] board) {
+	}
+	
+	public static int randomize() {
+		for(int i )
+	}
 }
